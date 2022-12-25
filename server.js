0f9c4f325812dcc1e02edef966148c56178e2625
Stefano Ghidini <s.ghidini@codermine.com>
2022-12-26 00:50:25 +0100

    V 0.0.1-beta.9

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ bot.on('message', (msg) => {
     const chatId = msg.chat.id;
   
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message'+String(chatId));
+    bot.sendMessage(chatId, 'chat_id: '+String(chatId));
 });
