3995615ca544a02ca562b7f5f97d1c14ff57a786
Stefano Ghidini <s.ghidini@codermine.com>
2023-03-20 09:59:26 +0100

    V 0.0.1-beta.16

--- API/git/git.service.js
+++ API/git/git.service.js
@@ -5,6 +5,8 @@ const { token, chat_id } = config.telegram;
 // Telegram
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {polling: true});
+// Terminal
+const { exec } = require("child_process");
 
 module.exports = {
     gitEvent
@@ -15,7 +17,11 @@ async function gitEvent(resp) {
 
     _sendTelegramMessage(resp.repository.name, resp.head_commit.committer.username, resp.head_commit.message)
 
-    return resp;
+    _updateStarBranch()
+
+    return {
+        "status": true
+    };
 }
 
 function _isStarBranch(ref) {
@@ -24,7 +30,7 @@ function _isStarBranch(ref) {
         return {
             "status": true,
             "message": "is not Star Branch"
-        }
+        };
     }
 }
 
@@ -32,4 +38,78 @@ function _sendTelegramMessage(repoName, committerUsername, commitMessage) {
     const message = `${committerUsername} pushed new ${commitMessage} to ${repoName}`;
 
     bot.sendMessage(chat_id, message);
+}
+
+function _updateStarBranch() {
+    exec("forever logs", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    exec("forever stopall", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    exec("~/PersonalGroupsSAPI git reset --hard HEAD", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    exec("~/PersonalGroupsSAPI git pull", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    exec("~/PersonalGroupsSAPI forever start server.js", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    exec("~/PersonalGroupsSAPI forever start smqttevents.js", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
 }
